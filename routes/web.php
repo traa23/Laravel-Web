0522ec765572facd2676d6d04bf57143a3dca661
putra <anugrah24si@mahasiswa.pcr.ac.id>
2025-10-01 08:01:38 +0700

    First commit...

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mahasiswa/{param1}', function () {
+    return "Mahasiswa epep";
+});
+
+Route::get('/nama/{param1}', function ($param1){
+    return "Nama saya : ".$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
